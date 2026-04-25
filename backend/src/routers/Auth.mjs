048e0b748f065dfcea4a1ca981727482c66fccd5
Rawaa Ahmed <rawaaahmed987@gmail.com>
2026-04-26 01:23:05 +0300

    feat : add validations on user

--- backend/src/routers/Auth.mjs
+++ backend/src/routers/Auth.mjs
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { handleLogin, handleRegister } from "../controllers/auth.mjs";
+import { handleLogin } from "../controllers/Auth/login.mjs";
+import { handleRegister } from "../controllers/Auth/register.mjs";
+import { checkSchema } from "express-validator";
+import { loginSchema, registerSchema } from "../utils/validationSchemas/user.mjs";
 
 const router = Router();
 
-router.post("/auth/login",handleLogin);
-router.post("/auth/register",handleRegister);
-
+router.post("/auth/login", checkSchema(loginSchema), handleLogin);
+router.post("/auth/register", checkSchema(registerSchema), handleRegister);
 export default router;
